bc2165c19c0566c3302ed2e1529b656a37680fc7
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-02-04 15:28:11 -0800

    feat: add ratings endpoint for retrieving movie ratings and register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.middlewares.headers import apply_response_headers
 from app.routes.health import health_bp
 from app.routes.movies import movies_bp
 from app.routes.links import links_bp
+from app.routes.ratings import ratings_bp
 
 def create_app():
     # Create Flask app
@@ -19,7 +20,8 @@ def create_app():
     app.register_blueprint(health_bp)
     app.register_blueprint(movies_bp)
     app.register_blueprint(links_bp)
-    
+    app.register_blueprint(ratings_bp)
+
     # Global error handling
     # @app.errorhandler(404)
     @app.errorhandler(405)
--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,6 @@
 from .health import health_bp
 from .movies import movies_bp
+from .links import links_bp
+from .ratings import ratings_bp
 
-__all__ = ["health_bp", "movies_bp"]
+__all__ = ["health_bp", "movies_bp", "links_bp", "ratings_bp"]
