0286e82c47b0e0a5ea65ffd5e5284ed871f1eb3c
Liang (Mario) Chen <liang.chen829@gmail.com>
2025-02-05 19:58:42 -0800

    fix: Uncomment healthcheck route in authentication middleware configuration

--- app/middlewares/auth.py
+++ app/middlewares/auth.py
@@ -6,7 +6,7 @@ from functools import wraps
 def configure_auth_middleware(app):
     # whitelist of routes that do not require authentication
     AUTH_EXEMPT_ROUTES = {
-        # 'healthcheck': {'methods': ['GET'], 'path': '/v1/healthcheck'},
+        'healthcheck': {'methods': ['GET'], 'path': '/v1/healthcheck'},
         'register': {'methods': ['POST'], 'path': '/v1/register'},
         'login': {'methods': ['POST'], 'path': '/v1/login'}
     }
